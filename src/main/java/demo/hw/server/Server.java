e202f72d7021ff54160b995fdd5a52b90b6d7977
liguoxiong <liguoxiong-it@bestpay.com.cn>
2017-08-18 17:05:15 +0800

    修改时间

--- src/main/java/demo/hw/server/Server.java
+++ src/main/java/demo/hw/server/Server.java
@@ -36,7 +36,7 @@ public class Server {
         new Server();
         System.out.println("Server ready...");
 
-        Thread.sleep(5 * 60 * 1000);
+        Thread.sleep(10 * 60 * 1000);
         System.out.println("Server exiting");
         System.exit(0);
     }
