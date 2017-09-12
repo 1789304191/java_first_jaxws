9591d99e52e8a2686f3969b1fa4b220652825b61
liguoxiong <liguoxiong-it@bestpay.com.cn>
2017-09-12 13:56:22 +0800

    work

---
+++ src/main/java/com/huateng/haobai/ppcore/control/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://control.ppcore.haobai.huateng.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.huateng.haobai.ppcore.control;
